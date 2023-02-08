21cae7e36860e60496cf2d903a0c1f7e8ac67d27
biasresearchltd <bias@biasresearch.ltd>
2023-02-07 23:10:25 -0800

    Aesthetic and other updates. Part 2.

--- src/components/RoboIcon.js
+++ src/components/RoboIcon.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import IconImage from "../icons/PUNKBOCOP.png";
 import styled from 'styled-components';
 
@@ -10,6 +10,18 @@ const Container = styled.div`
   margin: 0;
   padding: 0;
   image-rendering: pixelated;
+  transition: all 1 ease-in-out;
+  &:hover {
+    position: relative;
+    cursor: pointer;
+    box-shadow: inset 11px 11px #FCFCFE, inset -11px -11px #565656, 2px 2px #000000;
+    right: 4px;
+    transform: scale(1.03);
+  }
+  &:active {
+    cursor: pointer;
+    box-shadow: inset 0 0 10px #333;
+  }
 `;
 
 const Image = styled.img`
@@ -18,6 +30,13 @@ const Image = styled.img`
   height: 64px;
   user-drag: none;
   user-select: none;
+  &:hover {
+    box-shadow: inset 24px 24px #FCFCFE, inset -24px -24px #565656, 2px 2px #000000;
+    animation: all 0.33s ease-in-out;
+  }
+  &:active {
+    cursor: pointer;
+    box-shadow: inset 0 0 10px #333;
 `;
 
 const RoboIcon = ({ onClick }) => (
@@ -29,4 +48,3 @@ const RoboIcon = ({ onClick }) => (
 );
 
 export default RoboIcon;
-
