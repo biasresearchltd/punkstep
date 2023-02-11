f9e5339fe2e3c7c3bb2aa4b7cd1dc98f25c98ec6
michaelbelong <michaelbelong@gmail.com>
2023-02-11 14:20:03 -0800

    Damn.

---
+++ src/components/ScrollButton.js
@@ -0,0 +1,57 @@
+import React from "react";
+import Up from "../icons/PuNKStep-Up@2x.png"
+import Down from "../icons/PuNKStep-Down@2x.png"
+import styled from "styled-components";
+
+const IconImg = styled.img`
+  width: 9px;
+  height: 9px;
+  user-drag: none;
+  user-select: none;
+  image-rendering: pixelated;
+`;
+
+const IconContainer = styled.div`
+  width: ${props => props.iconSize};
+  height: ${props => props.iconSize};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledWindowButton = styled.div`
+  width: 16px;
+  height: 16px;
+  background-color: #AAA;
+  border: none;
+  outline: none;
+  cursor: default;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-shadow: inset 1px 1px #FCFCFE, inset -1px -1px #565656, 1px 1px #000000;
+  user-drag: none;
+  user-select: none;
+
+  margin: 3px;
+  &:hover {
+    background-color: #ddd;
+  }
+  &:active {
+    transform: scale(0.95);
+  }
+`;
+
+const WindowButton = ({ onClick, icon, iconSize = "10px" }) => (
+  <StyledWindowButton onClick={onClick}>
+    <IconContainer iconSize={iconSize}>
+      {icon === "up" ? (
+        <IconImg src={Up} alt="Minimize icon" />
+      ) : icon === "down" ? (
+        <IconImg src={Down} alt="Close icon" />
+      ) : null }
+    </IconContainer>
+  </StyledWindowButton>
+);
+export default WindowButton;
+
--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -1,18 +1,18 @@
 import React, { useRef, useEffect } from "react";
-import WindowButton from "./WindowButton";
+import ScrollButton from "./ScrollButton";
 import styled from 'styled-components';
 
 const Content = styled.div`
   position: relative;
   textarea {
-	width: calc(100% - 25px);
+	width: calc(100% - 26px);
 	height: 299px;
 	position: absolute;
-	left: 21px;
+	left: 22px;
   }
 `;
 
-const Scrollbar = styled.div`
+const ScrollbarContainer = styled.div`
   width: 22px;
   height: 304px;
   background-color: grey;
@@ -24,21 +24,46 @@ const Scrollbar = styled.div`
   flex-direction: column;
   justify-content: flex-end;
   align-items: center;
+  border: 1px solid black;
 
+  .scrollContainer {
+	position: absolute;
+	left: 1px;
+	top: 0;
+	width: 100%;
+  }
+  
   .scrollbar {
 	box-sizing: border-box;
 	position: absolute;
-	margin-left: -1px;
+	margin-left: 1px;
 	margin-top: 2px;
 	top: 0;
-	width: 15px;
+	width: 16px;
 	background-color: #AAAAAA;
 	box-shadow: inset 1px 1px #FCFCFE, inset -1px -1px #565656, 1px 1px #000000;
+	image-rendering: pixelated;
   }
   
-  .windowButton {
-	  padding: 0;
-	  margin: 0;
+  .buttonContainer {
+	position: absolute;
+	left: -1px;
+	width: 100%;
+  }
+  
+  .scrollButton {
+	position: absolute;
+	padding: 0;
+	margin: 0;
+	image-rendering: pixelated;
+  }
+  
+  .checkered3 {
+	background-color: #0075FF;
+	background-size: 2px 2px;
+	background-position: 0 0, 1px 1px;
+	background-image: linear-gradient(45deg, rgba(0, 0, 0, 0.85) 15%, transparent 25%, transparent 75%, rgba(0, 0, 0, 0.85) 66%, rgba(0, 0, 0, 0.85)),
+					  linear-gradient(45deg, rgba(0, 0, 0, 0.85) 15%, transparent 25%, transparent 75%, rgba(0, 0, 0, 0.85) 66%, rgba(0, 0, 0, 0.85));
   }
 `;
 
@@ -52,7 +77,7 @@ const TextArea = ({ text, handleTextChange }) => {
 	const scrollbar = scrollbarRef.current;
 	const updateScrollbar = () => {
 	  const barHeight = Math.min(textarea.offsetHeight * (textarea.offsetHeight / textarea.scrollHeight), textarea.offsetHeight);
-	  scrollbar.style.height = `${barHeight}px`;
+	  scrollbar.style.height = `${barHeight-44}px`;
 	  scrollbar.style.top = `${textarea.scrollTop * (textarea.offsetHeight / textarea.scrollHeight)}px`;
 	};
 
@@ -62,13 +87,23 @@ const TextArea = ({ text, handleTextChange }) => {
 	};
   }, []);
 
+  const handleUpClick = () => {
+	textareaRef.current.scrollTop -= 50;
+  };
+
+  const handleDownClick = () => {
+	textareaRef.current.scrollTop += 50;
+  };
+
   return (
 	<Content className="content">
-	  <Scrollbar>
-		<div ref={scrollbarRef} className="scrollbar"/>
-		<WindowButton className="up-button" icon="up" onClick="" />
-		<WindowButton className="down-button" icon="down" onClick="" />
-	  </Scrollbar>
+	  <ScrollbarContainer>
+		<div className="scrollContainer">
+		<div ref={scrollbarRef} className="scrollbar"/></div>
+		<div className="butttonContainer">
+		<ScrollButton className="scrollButton" icon="up" onClick={handleUpClick} />
+		<ScrollButton className="scrollButton" icon="down" onClick={handleDownClick} /></div>
+	  </ScrollbarContainer>
 	  <textarea
 		ref={textareaRef}
 		className="textedit-textarea"
@@ -81,3 +116,4 @@ const TextArea = ({ text, handleTextChange }) => {
 };
 
 export default TextArea;
+
